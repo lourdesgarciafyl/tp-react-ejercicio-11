bcd18b8db07a2bb260b36f0ecfe9f6338b132ee3
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-06-04 20:05:43 -0300

    se agrega el manejador de eventos para el form select

--- src/components/FormularioNoticias.jsx
+++ src/components/FormularioNoticias.jsx
@@ -1,16 +1,29 @@
 import { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Form, Row, Col } from "react-bootstrap";
 
 
-const FormularioNoticias = ({consultarApi }) => {
-    const [categoria, setCategoria] = useState("")
+const FormularioNoticias = ({ consultarApi }) => {
+    const [categoria, setCategoria] = useState("");
+
+    const handleOpciones = (event) =>{
+        const categoriaElegida = event.target.value;
+        setCategoria(categoriaElegida)
+        if(categoriaElegida === "--Seleccione una categoria--"){
+            return 
+        } else{
+            consultarApi(categoriaElegida)
+        }
+    }
 
     return (
         <section>
             <Form className="d-flex flex-column align-items-center flex-md-row justify-content-md-center">
-                <Form.Group>
+                <Form.Group as={Row}>
+                <Col md={5}>
                 <Form.Label className="mt-1">Buscar por categoría:</Form.Label>
-                <Form.Select  className="mx-2" id="formSelect">
+                </Col>
+                <Col md={6}>
+                <Form.Select  className="mx-2" id="formSelect" onChange={handleOpciones} value={categoria}>
                 <option value="">--Seleccione una categoria--</option>
                 <option value="top">Destacadas</option>
                 <option value="science">Ciencia</option>
@@ -19,6 +32,7 @@ const FormularioNoticias = ({consultarApi }) => {
                 <option value="politics">Politica</option>
                 <option value="technology">Tecnología</option>
                 </Form.Select>
+                </Col>
                 </Form.Group>
         </Form>
         </section>
