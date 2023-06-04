e8ae63dd6c3d453b4a6c30fb7c4bfef4c4420add
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-06-04 16:44:22 -0300

    se agregan carpetas de components y footer general

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+
 
 function App() {
 
   return (
     <>
+    <Container id='containerPrincipal'>
+      <h1 className="display-5 text-center my-3">Web de noticias</h1>
+        </Container>
+    <footer className='bg-dark text-light text-center py-3'>
+      <p>&copy; Todos los derechos reservados</p>
+    </footer>
     </>
   )
 }
