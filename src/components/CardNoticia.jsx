62679bb1a20aa19f3f97bdb6d6666e0f5d9d91d4
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-06-04 20:28:43 -0300

    se agregan los datos de cada noticia en las cards

--- src/components/CardNoticia.jsx
+++ src/components/CardNoticia.jsx
@@ -1,17 +1,18 @@
+import React from "react";
 import {Card, Col, Button} from "react-bootstrap"
 
-const CardNoticia = () => {
+const CardNoticia = ({ noticia }) => {
 
     return(
-        <Col>
-        <Card className="mb-2">
-            <Card.Img className="img-fluid" src="" alt=""></Card.Img>
+        <Col sm={12} md={4}>
+        <Card className="mb-1 mt-2">
+            <Card.Img variant="top" src={noticia.image_url} alt="imagen de la noticia"></Card.Img>
             <Card.Body>
-                <Card.Title>Noticia</Card.Title>
-                <h6 className="lead fs-6">Fuente:</h6>
+                <Card.Title>{noticia.title}</Card.Title>
+                <h6 className="lead fs-6">Fuente:{noticia.source_id}</h6>
                 <hr className="my-1"/>
-                <Card.Text>Descripcion</Card.Text>
-                <Button variant="primary">Ver noticia completa</Button>
+                <Card.Text className="descripcionNoticia">{noticia.description}</Card.Text>
+                <a variant="primary" href={noticia.link}>Ver noticia completa</a>
             </Card.Body>
         </Card>
         </Col>
